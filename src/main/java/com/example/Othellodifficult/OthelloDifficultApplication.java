042a5c50fc654f73297a8624225b1248b33a3031
newgate2601 <newgate2601@gmail.com>
2024-02-21 15:42:35 +0700

    feature: config maven mapper binding with lombok

--- src/main/java/com/example/Othellodifficult/OthelloDifficultApplication.java
+++ src/main/java/com/example/Othellodifficult/OthelloDifficultApplication.java
@@ -2,8 +2,10 @@ package com.example.Othellodifficult;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.Othellodifficult"})
 public class OthelloDifficultApplication {
 
 	public static void main(String[] args) {
