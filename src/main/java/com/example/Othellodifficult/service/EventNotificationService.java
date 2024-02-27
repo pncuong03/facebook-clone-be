83022cf1c088c8eb36a6f66117f86c218770cd83
newgate2601 <newgate2601@gmail.com>
2024-02-27 16:09:20 +0700

    .

--- src/main/java/com/example/Othellodifficult/service/EventNotificationService.java
+++ src/main/java/com/example/Othellodifficult/service/EventNotificationService.java
@@ -2,21 +2,16 @@ package com.example.Othellodifficult.service;
 
 import com.example.Othellodifficult.common.Common;
 import com.example.Othellodifficult.dto.event.EventNotificationOutput;
-import com.example.Othellodifficult.entity.ChatEntity;
-import com.example.Othellodifficult.entity.UserChatEntity;
 import com.example.Othellodifficult.entity.message.EventNotificationEntity;
 import com.example.Othellodifficult.repository.EventNotificationRepository;
 import com.example.Othellodifficult.token.TokenHelper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -37,7 +32,7 @@ public class EventNotificationService {
                             EventNotificationOutput.builder()
                                     .id(eventNotificationEntity.getId())
                                     .eventType(eventNotificationEntity.getEventType())
-                                    .content("comming soon !!!")
+                                    .content("coming soon !!!")
                                     .build()
                     );
                 }
